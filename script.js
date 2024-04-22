603bf7c384ea8f9158050c411916af9a0db5a534
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-22 22:07:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+var form= document.getElementById("book-form");
+form.addEventListener("submit", function(event){
+	event.preventDefault();
+
+	var title=document.getElementById("title").value;
+	var author=document.getElementById("author").value;
+	var isbn=document.getElementById("isbn").value;
+	console.log("Title:", title, "Author:", author, "ISBN:", isbn);
+})
