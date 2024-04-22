9abd38dd7489a66b38bc3eec6d4301884e8930d0
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-22 22:10:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var form= document.getElementById("book-form");
-form.addEventListener("submit", function(event){
-	event.preventDefault();
+// var form= document.getElementById("book-form");
+// form.addEventListener("submit", function(event){
+// 	event.preventDefault();
 
-	var title=document.getElementById("title").value;
-	var author=document.getElementById("author").value;
-	var isbn=document.getElementById("isbn").value;
-	console.log("Title:", title, "Author:", author, "ISBN:", isbn);
+// 	var title=document.getElementById("title").value;
+// 	var author=document.getElementById("author").value;
+// 	var isbn=document.getElementById("isbn").value;
+// 	console.log("Title:", title, "Author:", author, "ISBN:", isbn);
 })
